ad5ab69ed1fc08990f7258b3e061851786aa564c
sunagatov43 <79836948+sunagatov43@users.noreply.github.com>
2021-10-06 01:47:24 +0500

    Разработчик 3 изменил кодировку

--- src/main/java/calculator/OutputDeposit.java
+++ src/main/java/calculator/OutputDeposit.java
@@ -10,9 +10,9 @@ public class OutputDeposit extends GFrameBuilder{
 	}
 	public void SetData(String Text) // МЕТОД ДОБАВЛЯЕТ НАДПИСЬ НА Jframe
 	{
-		JLabel dohodLabel = new JLabel (Text); // Добавляем надпись
-		dohodLabel.setBounds(20,10,200,30); // задаем размер
-		super.add(dohodLabel); // добавляем лейбл на наш JFrame
+		JLabel dohodLabel = new JLabel (Text);  // Добавляем надпись
+		dohodLabel.setBounds(20,10,200,30);  // задаем размер
+		super.add(dohodLabel);  // добавляем лейбл на наш JFrame
 	}
 
 }
