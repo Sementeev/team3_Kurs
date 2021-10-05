e7b48d2025d4511d30ade028bcd2658f995a6c99
sunagatov43 <79836948+sunagatov43@users.noreply.github.com>
2021-10-05 17:38:00 +0500

    Разработчик 3 внес изменения в класс calculate

--- src/main/java/calculator/calculate.java
+++ src/main/java/calculator/calculate.java
@@ -27,8 +27,8 @@ public class calculate implements CalculateInterface{ // Создаем клас
 			e.printStackTrace();
 		}
 		    
-		ka = scan.next();//Логин пользователя
-		ko = scan.next();//Пароль пользователя
+		ka = scan.next();
+		ko = scan.next();
 		  
         scan.close();
         coef1 = Float.parseFloat(ka);
@@ -42,7 +42,7 @@ public class calculate implements CalculateInterface{ // Создаем клас
 	boolean isResident = false; 
 	boolean ychitivatNalog = false;
 	float H, T, a, D1,D2, C; // H- налог T-срок вклада a-процент D1-новая сумма вклада(БЕЗ УЧЕТА НАЛОГА) D2-новая сумма вклада(С УЧЕТОМ НАЛОГА)
-	String valuta = "Рубли"; //"Рубли" , "Доллары" , "Евро"
+	String valuta = "Рубли";  //"Рубли" , "Доллары" , "Евро"
 	@Override // Переопределяет метод интерфейса
 	public void CalculateNalog()
 	{
@@ -68,7 +68,7 @@ public class calculate implements CalculateInterface{ // Создаем клас
 		else
 			D2 = D1;
 	}
-	public calculate(String a_str, String T_str, String C_str, boolean resident, boolean _ychitivatNalog, String _valuta) // Конструктор
+	public calculate(String a_str, String T_str, String C_str, boolean resident, boolean _ychitivatNalog, String _valuta) 
 	{
 		this.valuta =_valuta;
 		this.ychitivatNalog = _ychitivatNalog;
@@ -76,40 +76,40 @@ public class calculate implements CalculateInterface{ // Создаем клас
 		if(!ParseAllData(a_str,T_str,C_str)) 
 		{
 			// данные не удалось сконвертировать -  ошибка ввода
-			OutputDeposit output = new OutputDeposit("Ошибка", 150, 50); //создаем всплывающее окно для вывода результатов
-			output.SetData("Ошибка ввода"); //создаем лейбл на нашем окне с текстом ошибки
-			return;
-		}
-		raschet();
-		CalculateDeposit();
-		OutputDeposit output = new OutputDeposit("Расчет", 250, 100);
-		output.SetData("Сумма вклада составил:" + String.valueOf(D2));
-	}
-	//проверяем все ли введенные данные являются числами (могут ли быть сконвертированны в Float и не пустые) и запоминаем
-	public boolean ParseAllData(String a_str, String T_str, String C_str) 
-	{
-		if(isFloat(a_str) && a_str.length()!=0)
-			a = Float.parseFloat(a_str)/100f;
-		else return false;
-		if(isFloat(T_str) && T_str.length()!=0)
-			T = Float.parseFloat(T_str);
-		else return false;
-		if(isFloat(C_str) && C_str.length()!=0)
-			C = Float.parseFloat(C_str);
-		else return false;
-		return true;
-	}
-	//может ли строка быть сконвертирована в Float
-	public boolean isFloat(String x) throws NumberFormatException
-	{
-	    try {
-	        Float.parseFloat(x);
-	        return true;
-	    } catch(Exception e) {
-	        return false;
-	    }
-	}
-	public float getFinalValue() {
-		return D2; //возвращает итоговое значение вклада
-	}
-}
+						OutputDeposit output = new OutputDeposit("Ошибка", 150, 50); //создаем всплывающее окно для вывода результатов
+						output.SetData("Ошибка ввода"); //создаем лейбл на нашем окне с текстом ошибки
+						return;
+					}
+		            raschet();
+					CalculateDeposit();
+					OutputDeposit output = new OutputDeposit("Расчет", 250, 100);
+					output.SetData("Сумма вклада составил:" + String.valueOf(D2));
+				}
+				//проверяем все ли введенные данные являются числами (могут ли быть сконвертированны в Float и не пустые) и запоминаем
+				public boolean ParseAllData(String a_str, String T_str, String C_str) 
+				{
+					if(isFloat(a_str) && a_str.length()!=0)
+						a = Float.parseFloat(a_str)/100f;
+					else return false;
+					if(isFloat(T_str) && T_str.length()!=0)
+						T = Float.parseFloat(T_str);
+					else return false;
+					if(isFloat(C_str) && C_str.length()!=0)
+						C = Float.parseFloat(C_str);
+					else return false;
+					return true;
+				}
+				//может ли строка быть сконвертирована в Float
+				public boolean isFloat(String x) throws NumberFormatException
+				{
+				    try {
+				        Float.parseFloat(x);
+				        return true;
+				    } catch(Exception e) {
+				        return false;
+				    }
+				}
+				public float getFinalValue() {
+					return D2; //возвращает итоговое значение вклада
+				}
+			}
