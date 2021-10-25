362e8d044989b3cecee862c07da36f4e5d9ce0d4
Sementeev <79837632+Sementeev@users.noreply.github.com>
2021-10-25 23:03:51 +0500

    Update JFrameFormTest.java
    Change test

--- src/test/java/calculator/JFrameFormTest.java
+++ src/test/java/calculator/JFrameFormTest.java
@@ -7,22 +7,20 @@ import org.junit.Test;
 
 import Calcul.JFrameForm;
 
-//òåñò àáñòðàêòíîãî êëàññà
 public class JFrameFormTest {
 
 	TestClass testClass;
 	   @Before
 	    public void setUp() throws Exception {
-		   testClass = new TestClass("test", 0, 0); //òóò âñå äåëàåò àáñòðàêòíûé êëàññ
+		   testClass = new TestClass("test", 0, 0); // // я создал тестовый класс котоыйр расширяет мой абстреатрйы класс JFrameBuilder и задается title  
 	    }
 
 	    @Test
 	    public void test() {
-	        assertTrue("test".equals(testClass.getTitle())); // ïðîâåðÿåì ÷òî íàçâàíèå çàäàëîñü âåðíî
+	        assertTrue("test".equals(testClass.getTitle())); // проверяю то что название совпадает (абстракстый класс сработал верно) 
 	    }
 }
 
-//ñîùäàåì êëàññ íàñëåäóåìûé îò àáñòðàêòíîãî
 class TestClass extends JFrameForm{
 
 	public TestClass(String _title, int width, int height) {
@@ -30,5 +28,6 @@ class TestClass extends JFrameForm{
 		// TODO Auto-generated constructor stub
 	}
 
-}
 
+
+}
