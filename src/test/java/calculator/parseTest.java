3c40a202fb41b358f8fb8ca981a2b29e236b3862
sunagatov43 <79836948+sunagatov43@users.noreply.github.com>
2021-10-25 18:33:58 +0500

    Разработчик 3 создал тест

--- src/test/java/calculator/parseTest.java
+++ src/test/java/calculator/parseTest.java
@@ -1,21 +1,23 @@
-/*
-package Calcul;
+package calculator;
 
 import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import Calcul.Calculate;
+
 public class parseTest {
 	Calculate calc;
 	@Before
     public void setUp() throws Exception {
-		calc = new Calculate("10", "6", "10000", true, true); // ñîçäàåì ýêçåìïëÿð êëàññà calculate - âõîäíûå äàííûå íå èèìåþò çíà÷åíèÿ
+		calc = new Calculate("10", "6", "10000", true, true, "Rubli"); //создаем экземпляр класса для работы с ним
     }
 
     @Test
     public void test() {
-        assertTrue(calc.ParseAllData("10", "0.023f", "010.2")); // óáåæäàåìñÿ ÷òî äàííûå ìîãóò áûòü êîíâåðòèðîâàííû â Float
+        assertTrue(calc.ParseAllData("10", "0.023f", "010.2")); // проверим парсятся ли данные (тру если парс прошел)
+        assertTrue(!calc.ParseAllData("фыв", "0.023f", "010.2")); // проверим парсятся ли данные (не тру, тк мы передаем букавки)
     }
+
 }
-*/
